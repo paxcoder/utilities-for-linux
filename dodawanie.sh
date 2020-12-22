e7244c3dd3e796607473ad527df42e0eed5af567
Patryk <61616935+paxcoder@users.noreply.github.com>
2020-12-22 19:14:42 +0100

    added multiplication script, #2
    edited allso other scripts, small inprovements.

--- dodawanie.sh
+++ dodawanie.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #definiujemy, gdzie skrypt ma się wykonać(shell), to jest skrypt basha, więc rozkażemy linuxowi aby to wykonał w bashu, bo inne shelle może to wykonają, ale mogą być błędy.
 echo "witaj w moim dodającym skrypcie"
-echo "wpisz pierwszą liczbę"
-#ekran powitalny
+echo "wpisz pierwszą liczbę" #ekran powitalny
 read a #mówimy bashowi, użytkownik ma wpisać wartość, w tym przypadku jest to a.
 echo "drugą liczbę" #po wczytaniu pierwszej zmiennej, mówimy użytkownikowi przez basha, aby wpisał drugą liczbę.
 read b #każemy bashowi wczytać kolejną zmienną, kturą użytkownik poda.
