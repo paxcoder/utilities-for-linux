6ba4c58281e86d09ba3f312460c16fd51702a9a4
Patryk <61616935+paxcoder@users.noreply.github.com>
2020-12-22 18:18:40 +0100

    adds comments in the code

--- addition.sh
+++ addition.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echo "welcome in my script, which makes  addittion"
-echo "write first number"
+#it defines an interpreter
+echo "welcome in my script, which adds 2 number  "
+echo "write first number"#welcoming screen
 read a
+#the variable which will be inputted by user
 echo "second number"
 read b
+#next variable.
 let wynik=$a+$b
+#makes variable wynik, which is equal  a+b
 echo $wynik
+#shows the wynik variable
